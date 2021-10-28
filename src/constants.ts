54188d9602913d45864c71ef8baaa2baa14f5098
Leon Linhart <themrmilchmann@gmail.com>
2021-10-28 14:10:49 +0200

    fix: fixup main script location and rename path-export options

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const INPUT_ARCHITECTURE = "arch";
 export const INPUT_VS_PATH = "vs-path";
 
-export const INPUT_EXPORT_VCVARSALL = "export-vcvarsall";
-export const INPUT_EXPORT_VS = "export-vs";
+export const INPUT_EXPORT_VCVARSALL = "export-path-to-vcvarsall";
+export const INPUT_EXPORT_VS = "export-path-to-vs";
