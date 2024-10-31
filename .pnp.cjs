8fc4bc5df72c64d97cf35175ced263d3532d315f
Leon Linhart <themrmilchmann@gmail.com>
2024-10-31 09:45:28 +0100

    build: refresh Yarn cache

--- .pnp.cjs
+++ .pnp.cjs
@@ -29,7 +29,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["@actions/core", "npm:1.11.1"],\
           ["@actions/github", "npm:6.0.0"],\
-          ["@types/node", "npm:22.8.4"],\
+          ["@types/node", "npm:20.17.4"],\
           ["@vercel/ncc", "npm:0.38.2"],\
           ["typescript", "patch:typescript@npm%3A5.6.3#optional!builtin<compat/typescript>::version=5.6.3&hash=8c6c40"]\
         ],\
@@ -300,10 +300,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@types/node", [\
-      ["npm:22.8.4", {\
-        "packageLocation": "./.yarn/cache/@types-node-npm-22.8.4-6e991f4f0e-ee6b6b66c2.zip/node_modules/@types/node/",\
+      ["npm:20.17.4", {\
+        "packageLocation": "./.yarn/cache/@types-node-npm-20.17.4-32c0bd3497-f4658e8982.zip/node_modules/@types/node/",\
         "packageDependencies": [\
-          ["@types/node", "npm:22.8.4"],\
+          ["@types/node", "npm:20.17.4"],\
           ["undici-types", "npm:6.19.8"]\
         ],\
         "linkType": "HARD"\
@@ -1272,7 +1272,7 @@ const RAW_RUNTIME_STATE =
           ["setup-msvc-dev", "workspace:."],\
           ["@actions/core", "npm:1.11.1"],\
           ["@actions/github", "npm:6.0.0"],\
-          ["@types/node", "npm:22.8.4"],\
+          ["@types/node", "npm:20.17.4"],\
           ["@vercel/ncc", "npm:0.38.2"],\
           ["typescript", "patch:typescript@npm%3A5.6.3#optional!builtin<compat/typescript>::version=5.6.3&hash=8c6c40"]\
         ],\
