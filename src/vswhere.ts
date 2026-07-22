ecbdaf90cc13d5d9f2117f2bfbc5921e43a0de91
Leon Linhart <themrmilchmann@gmail.com>
2026-07-22 23:51:06 +0200

    feat: implement vswhere lookup
    Closes #490

--- src/vswhere.ts
+++ src/vswhere.ts
@@ -20,14 +20,30 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import * as core from "@actions/core";
 import * as child from "child_process";
+import * as fs from "fs";
 
 export function findVS(): string {
+    let vswhere = findVSWhere();
+
     try {
-        return child.execSync("vswhere -products * -latest -prerelease -property installationPath")
+        return child.execSync(`"${vswhere}" -products * -latest -prerelease -property installationPath`)
             .toString()
             .trim();
     } catch (e) {
         throw new Error("vswhere did not find Visual Studio", { cause: e });
     }
+}
+
+function findVSWhere(): string {
+    const programFilesX86 = process.env["ProgramFiles(x86)"];
+    const vswherePath = `${programFilesX86}\\Microsoft Visual Studio\\Installer\\vswhere.exe`;
+    if (fs.existsSync(vswherePath)) {
+        core.info(`Found vswhere at '${vswherePath}'`);
+        return vswherePath;
+    }
+
+    core.info("Assuming vswhere is available in %PATH%");
+    return "vswhere";
 }
